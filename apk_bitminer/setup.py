1e9df141398cef3ae271cc2b2fe73418fb928e25
john rusnak <jrusnak@linkedin.com>
2018-08-13 11:03:31 -0700

    version bump

--- apk_bitminer/setup.py
+++ apk_bitminer/setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     include_package_data=True,
     namespace_packages=[],
     license='BSD 2-CLAUSE LICENSE',
-    version='1.0.2',
+    version='1.0.3',
     scripts=['src/apk_bitminer/pydexdump',
              'src/apk_bitminer/pyaxmldump']
 )
